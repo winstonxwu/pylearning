7d6fbfa9b97ca674f72376835503ac88ba5085c0
Shawny2501 <160569957+Shawny2501@users.noreply.github.com>
2024-10-09 14:42:26 -0400

    Variables

---
+++ Lessons/variables.py
@@ -0,0 +1,40 @@
+# Integer
+int_var = 10
+print(int_var)
+
+# Floating-point number
+float_var = 10.5
+print(float_var)
+
+# Complex number
+complex_var = 3 + 4j
+print(complex_var)
+
+# String
+str_var = "Hello, World!"
+print(str_var)
+
+# Boolean
+bool_var = True
+print(bool_var)
+
+
+# List
+list_var = [1, 2, 3, 4, 5]
+print(list_var)
+
+# Tuple
+tuple_var = (1, 2, 3, 4, 5)
+print(tuple_var)
+
+# Set
+set_var = {1, 2, 3, 4, 5}
+print(set_var)
+
+# Dictionary
+dict_var = {"name": "Alice", "age": 25}
+print(dict_var)
+
+# NoneType
+none_var = None
+print(none_var)
